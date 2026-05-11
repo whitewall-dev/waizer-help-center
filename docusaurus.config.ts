be6cfaa35d7a0f2d22ae64f69e35f14919428440
BrunoCamarda <bruno.camarda@gmail.com>
2026-05-11 10:54:34 -0300

    change docussaurus url config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
     v4: true,
   },
 
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://helpwaizer.netlify.app',
   baseUrl: '/',
 
   organizationName: 'waizer', // Atualizado
